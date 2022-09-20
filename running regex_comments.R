fdd32ee93a68faacc0920425e5f40a807a1b7b4d
Mkranj <111303584+Mkranj@users.noreply.github.com>
2022-09-20 09:22:43 +0200

    Rewritten code for readability
    Removed redundancies and inconsistencies, rewritten variable names to be more intuitive.

--- running regex_comments.R
+++ running regex_comments.R
@@ -1,120 +1,112 @@
 library(tidyverse)
 library(stringr)
 
-# read the text file containing dates and information about the runs, as hand-written.
+# Read the text file containing dates and information about the runs, as hand-written.
 original <- read.delim(file = "trcanje info.txt", header = F, encoding = "UTF-8")
 
-# Experimenting with regex ----
-# finding out the patterns suitable for extractions
+# Extracting dates and numerical data with Regex ----
+# Finding out the patterns suitable for extractions
 # 
-# first, we remove the rows that have no date - these are the location indicators and other notes
+# First, we remove the rows that have no date - these are the location indicators and other notes
 # Variables: date, run time, run distance (in laps)
-# date is standard:dd.mm.yy format
-
-str_view_all(original, "kr")
-
-
-test_vec <- original$V1[c(1:5, 250:260, 270:272, 111)]
-
-str_view_all(test_vec, "\\d\\d\\.\\d\\d\\.\\d\\d") # date
-
-str_view_all(test_vec, "\\d\\d")
-# two numbers with zero or more spaces, then "m"
-# another extra :dd is possible (with times of over an hour)
-# that's an alternate possibility, indicated |
-str_view_all(test_vec, "(\\d\\d\\:\\d\\d.*m?)|(\\d+\\s*m(in)*)") #run time
-
-# lap numbers end with "kr"
-str_view_all(test_vec, "\\d+(\\.\\d)*\\s*kr")
+# Date is standard:dd.mm.yy format
 
 
 
-# work on full base ----
-str_view_all(original$V1, "\\d\\d\\.\\d\\d\\.\\d\\d") # date
-str_view_all(original$V1, "(\\d\\d\\:\\d\\d.*m?)|(\\d+\\s*m(in)*)") # run time
-str_view_all(original$V1, "\\d+(\\.\\d)*\\s*kr") # laps
 
-# extract dates
-str_extract(test_vec, "\\d\\d\\.\\d\\d\\.\\d\\d")
-str_extract(test_vec, "\\d+\\.\\d+\\.\\d\\d")
+# Extract dates
 
-dates_1 <- str_extract(original$V1, "\\d+\\.\\d+\\.\\d\\d")
-dates_2 <- as.Date(dates_1, format="%d.%m.%y")
+regex_date_pattern <- "\\d+\\.\\d+\\.\\d\\d"
+extracted_dates <- str_extract(original$V1, regex_date_pattern) %>% as.Date(extracted_dates, format="%d.%m.%y")
 
-# extract time
-times_1 <- str_extract(original$V1, "(\\d\\d\\:\\d\\d.*m?)|(\\d+\\s*m(in)*)")
-# works well
+# Extract laps
 
+regex_lap_pattern <- "\\d+([\\.\\,]\\d)*\\s*kr"
+regex_lap_pattern_just_number <-  "^\\d+([\\.\\,]\\d)*"
+# First we'll extract the whole part describing number of laps ran,
+# Then just the number
 
-# extract laps
-laps_1 <- str_extract(original$V1,"\\d+([\\.\\,]\\d)*\\s*kr")
-laps_2 <- str_extract(laps_1,"^\\d+([\\.\\,]\\d)*")
-laps_2 <- as.numeric(laps_2)
+extracted_laps <- str_extract(original$V1, regex_lap_pattern) %>% str_extract(regex_lap_pattern_just_number) %>%
+    as.numeric()
 
+# To extract time, we first extract a vector of strings like "mm:ss", 
+# Then turn that into a number of seconds in a new vector
 
-
-# to extract time, we first extract a vector of strings like "mm:ss", then turn that into a number of second in a second vector
-# extract time
-times_1 <- str_extract(original$V1,"(\\d\\d\\:\\d\\d.*m?)|(\\d+\\s*m(in)*)")
-times_mins <- str_extract(times_1,"^\\d+") # 100 is from "100m" notes, doesn't represent time
-times_mins2 <- times_mins
-times_mins2[which(times_mins2 == "100")] <- NA
-times_mins2 <- as.numeric(times_mins2)
-times_secs <- str_extract(times_1,"\\:\\d\\d")
-times_secs2 <- str_extract(times_secs,"\\d\\d")
-times_secs2 <- as.numeric(times_secs2)
+regex_time_ran_pattern <- "(\\d\\d\\:\\d\\d.*m?)|(\\d+\\s*m(in)*)"
+# Two numbers with zero or more spaces, then "m"
 
 
-times_hours <- str_extract(original$V1, "\\d\\:\\d\\d:\\d\\d.")
-times_hours2 <- str_extract(times_hours,"^\\d") # in those rows that contain a number of hours, the FIRST digit is the number of hours
-times_hours2 <- as.numeric(times_hours2)
-times_hours_minutespart <- str_extract(times_hours, "\\:\\d\\d:\\d\\d")
-times_hours_minutes_sec <- str_extract(times_hours_minutespart, "\\d\\d:\\d\\d")
-times_hours_minutes1 <- str_extract(times_hours_minutes_sec, "^\\d+")
-times_hours_minutes1 <- as.numeric(times_hours_minutes1)
 
-times_hours_sec1 <- str_extract(times_hours_minutes_sec, ":\\d\\d")
-times_hours_sec2 <- str_extract(times_hours_sec1," \\d\\d")
-times_hours_sec2 <- as.numeric(times_hours_sec2)
+extracted_times <- str_extract(original$V1,regex_time_ran_pattern)
 
-# add hours to minutes
-hourtime_mins <- times_hours2 * 60 + times_hours_minutes1
-hourtime_secs <- times_hours_sec2
+# Extract number of minutes
+just_minutes <- str_extract(extracted_times,"^\\d+")
+# 100 is from "100m" notes, doesn't represent time - delete
+just_minutes[which(just_minutes == "100")] <- NA
+just_minutes <- as.numeric(just_minutes)
+
+# Extract number of seconds
+just_seconds <- str_extract(extracted_times,"\\:\\d\\d") %>% str_extract("\\d\\d") %>% as.numeric()
+
+# Extract number of hours
+regex_time_ran_pattern_more_than_hour <- "\\d\\:\\d\\d:\\d\\d."
+just_hours <- str_extract(original$V1, regex_time_ran_pattern_more_than_hour) %>% str_extract("^\\d") %>% as.numeric()
+# In those rows that contain a number of hours, the FIRST digit is the number of hours
+
+# Get the full text for display in database, replace those parts in extracted_times
+# Because they only caught the minutes and seconds, not the hours
+
+extracted_times_more_than_hour <- str_extract(original$V1, regex_time_ran_pattern_more_than_hour)
+extracted_times_more_than_hour_row_number <- which(!is.na(extracted_times_more_than_hour))
+
+# Update the text info for these runs
+extracted_times[extracted_times_more_than_hour_row_number] <- extracted_times_more_than_hour[extracted_times_more_than_hour_row_number]
+
+# Get minutes from these parts too 
+more_than_hour_text <- str_extract(original$V1, regex_time_ran_pattern_more_than_hour) %>% 
+    str_extract("\\:\\d\\d:\\d\\d") %>%  str_extract("\\d\\d:\\d\\d") 
+more_than_hour_minutes <- more_than_hour_text %>% str_extract("^\\d+") %>% as.numeric()
+more_than_hour_seconds <- more_than_hour_text %>% str_extract(":\\d\\d") %>% str_extract("\\d\\d") %>% as.numeric()
+
+# Add hours to minutes
+more_than_hour_minutes <- just_hours * 60 + more_than_hour_minutes
+
   
-# add the times with hours to the regulars mins and secs
-hourtime_which <- which(!is.na(hourtime_mins))
+# Add the times with hours to the regulars mins and secs
+which_times_more_than_hour <- which(!is.na(more_than_hour_minutes))
 
-times_mins2[hourtime_which] <- hourtime_mins[hourtime_which]
-times_secs2[hourtime_which] <- hourtime_secs[hourtime_which]
-
-
-# minutes to seconds and add the two
-data_times <- data.frame(ortime = times_1, min = times_mins2, secs_only = times_secs2)
-data_times <- mutate(data_times, secs_calc = replace_na(secs_only, 0),mintosec=min * 60 + secs_calc)
-
-# round up minutes for seconds > 31
-data_times$min_round <- data_times$min
-data_times$min_round[which(data_times$secs_calc > 30)] <- data_times$min[which(data_times$secs_calc > 30)] + 1
-
-data_times_to_join <- select(data_times, ortime, min_round, min,secs_only, mintosec)
-
-all_data <- data.frame(original_text=original$V1, date = dates_2, laps = laps_2)
-all_data <- cbind(all_data, data_times_to_join)
+just_minutes[which_times_more_than_hour] <- more_than_hour_minutes[which_times_more_than_hour]
+just_seconds[which_times_more_than_hour] <- more_than_hour_seconds[which_times_more_than_hour]
 
 
-# remove rows with less than 10 mins and which have no time indicated
-all_data[which(all_data$min_round < 11), "min_round"] <- NA
-all_data <- all_data[which(is.na(all_data$min_round)==F), ]
+# Minutes to seconds and add the two
+data_times <- data.frame(time_original_text = extracted_times, min = just_minutes, sec_only = just_seconds)
+data_times <- mutate(data_times, sec_only = replace_na(sec_only, 0), total_seconds = min * 60 + sec_only)
 
-rownames(all_data) <- NULL #cleaning up
-all_data <- all_data[which(is.na(all_data$date) == F), ]
+# Round up minutes for seconds > 31
+data_times$min_rounded <- data_times$min
+data_times$min_rounded[which(data_times$sec_only > 30)] <- data_times$min[which(data_times$sec_only > 30)] + 1
+
+# Reordering the columns
+data_times <- select(data_times, time_original_text, min_rounded, min,sec_only, total_seconds)
+
+# Joining with text, date and lap info
+running_data <- data.frame(original_text=original$V1, date = extracted_dates, laps = extracted_laps)
+running_data <- cbind(running_data, data_times)
+
+# Remove rows with less than 10 mins and which have no time indicated
+running_data[which(running_data$min_rounded < 11), "min_rounded"] <- NA
+running_data <- running_data[which(is.na(running_data$min_rounded)==F), ]
+
+running_data <- running_data[which(is.na(running_data$date) == F), ]
+rownames(running_data) <- NULL # We don't need the original rowname numbers
 
 # Extracting location ----
-# Runs were recorded in two places, Koprivnica and Zagreb. Laps in Koprivnica (KC) are 300m, in Zagreb they're 400m.
-# So we wanted to extract the location data for each run, which will also allow us to calculate run distance accurately.
+# Runs were recorded in two places, Koprivnica and Zagreb. Laps in Koprivnica (KC) are 300m, in Zagreb (ZG) they're 400m.
+# So we want to extract the location data for each run, which will also allow us to calculate run distance accurately.
 
 # How we're going about it:
-# locations are named as specific rows in the text. All runs until the next location is mentioned are presumed to be in that location.
+# Locations are named as specific rows in the text. All runs until the next location is mentioned are presumed to be in that location.
 # Locate dates when those locations are named, all following runs will have that location applied to them.
 # If a further location change exists, it will overwrite the location info in the next pass, keeping it all up to date!
 
@@ -132,10 +124,11 @@ kc_1 <- str_which(locations_text$V1, "KOPRIVNICA|KC")
 zg_1 <- str_which(locations_text$V1, "ZAGREB|ZG")
 
 # Finding dates - in the same rows, ones below and ones one more below
-dates_kc1 <- dates_2[kc_1]
-dates_kc2 <- dates_2[kc_1 + 1]
-dates_kc3 <- dates_2[kc_1 + 2]
+dates_kc1 <- extracted_dates[kc_1]
+dates_kc2 <- extracted_dates[kc_1 + 1]
+dates_kc3 <- extracted_dates[kc_1 + 2]
 
+# New dataframe just for Koprivnica locations - we'll do the same for Zagreb, combine them and add it to the main df later
 df_location_dates <- data.frame(location = rep("KOPRIVNICA", length(dates_kc1)))
 
 # Put the ones found in the same line in a dataframe.
@@ -144,98 +137,84 @@ df_location_dates$date <- dates_kc1
 # If it's not in the same row, try the next. if it's still empty, try the next.
  
 for (i in 1 : nrow(df_location_dates)) {
-if (is.na(df_location_dates$date[i])) {
-  df_location_dates$date[i] <- dates_kc2[i]
-}  
-
+  if (is.na(df_location_dates$date[i])) {
+    df_location_dates$date[i] <- dates_kc2[i]
 }
-
-for (i in 1 : nrow(df_location_dates)) {
+  # Still empty? Try the n+2 row
   if (is.na(df_location_dates$date[i])) {
     df_location_dates$date[i] <- dates_kc3[i]
   }  
-  
-}
+}  
 
 # That's it for Koprivnica, repeat the same for Zagreb.
 
-dates_zg1 <- dates_2[zg_1]
-dates_zg2 <- dates_2[zg_1 + 1]
-dates_zg3 <- dates_2[zg_1 + 2]
+dates_zg1 <- extracted_dates[zg_1]
+dates_zg2 <- extracted_dates[zg_1 + 1]
+dates_zg3 <- extracted_dates[zg_1 + 2]
 
+# Dataframe of locations and dates they occur
 df_location_dates_zg <- data.frame(location = rep("ZAGREB", length(dates_zg1)))
 
 df_location_dates_zg$date <- dates_zg1
 
+# Same as for Koprivnica
 for (i in 1 : nrow(df_location_dates_zg)) {
   if (is.na(df_location_dates_zg$date[i])) {
     df_location_dates_zg$date[i] <- dates_zg2[i]
   }  
-  
-}
-
-for (i in 1 : nrow(df_location_dates_zg)) {
   if (is.na(df_location_dates_zg$date[i])) {
     df_location_dates_zg$date[i] <- dates_zg3[i]
-  }  
-  
+  } 
 }
 
-df_location_dates <- rbind(df_location_dates, df_location_dates_zg)
+# Bind them together, arrange by date
+df_location_dates <- rbind(df_location_dates, df_location_dates_zg) %>% arrange(date)
 df_location_dates <- arrange(df_location_dates, date)
 
 # And now to apply it to the main database. The first row is in Zagreb,
-# Apply that to everything as the default. It will be changed where needed.
+# Apply that to everything as the default. The df_location_dates marks when locations change -
+# From that point to the next, every run is in the location named.
 
-all_data$location <- "ZAGREB"
+running_data$location <- "ZAGREB"
 
 for (i in 1 : nrow(df_location_dates)) {
   # This for goes through every date that has a location change
-  for (j in 1 : nrow(all_data)) {
+  for (j in 1 : nrow(running_data)) {
    # For every row of the main base checks if the date is equal or higher than the current location date
-   # if so, it changes the location
-     if (all_data$date[j] >= df_location_dates$date[i]) {
-       all_data$location[j] <- df_location_dates$location[i]}
+   # If so, it changes the location
+     if (running_data$date[j] >= df_location_dates$date[i]) {
+       running_data$location[j] <- df_location_dates$location[i]}
   }
 }
 
+# Calculating distance and speed ----
 
-# It works! and now the main part...
-
-# Calculate distance, and speed in km/h
-data_calculations <- mutate(all_data, distance_km = laps * 0.4)
+running_data <- mutate(running_data, distance_km = laps * 0.4)
 
 # Koprivnica distances are 3/4ths as big
-for (i in 1: nrow(data_calculations)) {
-  if (data_calculations$location[i] == "KOPRIVNICA") {data_calculations$distance_km[i] = data_calculations$distance_km[i] * 3/4}
+for (i in 1: nrow(running_data)) {
+  if (running_data$location[i] == "KOPRIVNICA") {
+    running_data$distance_km[i] = running_data$distance_km[i] * 3/4
+    }
 }
 
-data_calculations <- mutate(data_calculations, km_h = distance_km / (mintosec / 3600))
+running_data <- mutate(running_data, km_h = distance_km / (total_seconds / 3600))
 
 
 # Graphs----- 
 
-ggplot(data_calculations, aes(x = date, y = km_h)) + geom_point() + geom_smooth(se = F, color = "red")
+ggplot(running_data, aes(x = date, y = km_h)) + geom_point() + geom_smooth(se = F, color = "red")
 ggsave(filename = "kmh map.png",width = 20)
-ggplot(data_calculations, aes(x = date, y = distance_km)) + geom_point() + geom_smooth(se = F, color = "red")
+ggplot(running_data, aes(x = date, y = distance_km)) + geom_point() + geom_smooth(se = F, color = "red")
 ggsave(filename = "distance map.png", width = 20)
-ggplot(data_calculations, aes(x = date, y = min_round)) + geom_point() + geom_smooth(se = F, color = "red")
+ggplot(running_data, aes(x = date, y = min_rounded)) + geom_point() + geom_smooth(se = F, color = "red")
 ggsave(filename = "time spent map.png", width = 20)
 
 # Data only on runs with number of laps
 
-data_only_with_laps <- filter(data_calculations, is.na(laps) == F)
-ggplot(data_only_with_laps, aes(x = date, y = min_round)) + geom_point() + geom_smooth(se = F, color = "red")
+data_only_with_laps <- filter(running_data, is.na(laps) == F)
+ggplot(data_only_with_laps, aes(x = date, y = min_rounded)) + geom_point() + geom_smooth(se = F, color = "red")
 ggsave(filename = "time spent map_only with laps.png", width = 20)
 
-# Calculating a new measure for fun: Running points. Basically, they reward going fast, and going for longer.
-# A run with higher speed which went over a longer distance is better exercise, right?
-# So far, it's simply the product of km/h and distance, so running 10 km at 10km/h would be worth 100 points.
-
-data_calculations <- mutate(data_calculations, running_points = km_h * distance_km)
-
-ggplot(data_calculations, aes(x = date, y  =running_points)) + geom_point() + geom_smooth(se = F, color = "red")
-ggsave(filename = "running points.png", width = 20)
-
 # Save dataframe as excel file, with today's date appended
-writexl::write_xlsx(data_calculations, path = paste("Running data", format(Sys.time(), "%Y-%m-%d"), ".xlsx"))
+writexl::write_xlsx(running_data, path = paste("Running data", format(Sys.time(), "%Y-%m-%d"), ".xlsx"))
